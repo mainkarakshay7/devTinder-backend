758044ba0c8cfaaa696e7dba5a470a2f065bf065
Akshay Mainkar <mainkarakshay7@gmail.com>
2025-05-09 00:25:41 +0530

    adding env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,8 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://akshay-ma:namastedev55@namastenode.vizfh.mongodb.net/devTinder"
-  );
+  await mongoose.connect(process.env.MONGO_URL);
 };
 
 module.exports = connectDB;
